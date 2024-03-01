7a20831d173b7b3b6d2ea19dd8ef868f81bc3112
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-03-01 22:39:48 +0100

    projects section, styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="w-full pb-[100px]">
       <Navbar />
       <Home />
+      <Projects />
       <Contact />
     </div>
   );
